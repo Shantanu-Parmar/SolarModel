0d7cc3e41328ebdc6f0b1db20560aa369ba1bede
Shantanusinh Parmar <113082290+Shantanu909@users.noreply.github.com>
2024-04-06 22:22:08 +0530

    Add files via upload

---
+++ sp.js
@@ -0,0 +1,16 @@
+// script.js
+let scene, camera, renderer;
+let sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune;
+let asteroids = [];
+let trajectoryPoints = [];
+let trajectoryIndex = 0;
+
+function init() {
+    // Your initialization code here
+}
+
+function animate() {
+    // Your animation code here
+}
+
+// Rest of the JavaScript code
